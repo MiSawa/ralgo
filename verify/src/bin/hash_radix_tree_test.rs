d8767520f02fd9124b2c3483931ffe127bd4fbcc
Ryo Yamashita <qryxip@gmail.com>
2020-08-20 16:04:58 +0900

    Do not explicitly specify `STDIN_SOURCE`

--- verify/src/bin/hash_radix_tree_test.rs
+++ verify/src/bin/hash_radix_tree_test.rs
@@ -4,29 +4,24 @@ use ralgo::data_structure::hash_radix_tree::HashRadixTree;
 // verify-helper: PROBLEM https://judge.yosupo.jp/problem/associative_array
 
 fn main() {
-    let mut source = proconio::STDIN_SOURCE.lock().unwrap();
     input! {
-        from &mut *source,
-        q: usize
+        q: usize,
     };
 
     let mut tree = HashRadixTree::new();
 
     for _ in 0..q {
         input! {
-            from &mut *source,
-            q_type: usize
+            q_type: usize,
         }
         if q_type == 0 {
             input! {
-                from &mut *source,
                 k: usize,
                 v: usize,
             }
             tree.insert(k, v);
         } else {
             input! {
-                from &mut *source,
                 k: usize,
             }
             println!("{}", tree.get(&k).unwrap_or(&0));
