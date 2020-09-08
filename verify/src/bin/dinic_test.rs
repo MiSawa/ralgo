6950ef6fcd281d35e2969c46e0d823fb56728761
mi_sawa <mi.sawa.1216+git@gmail.com>
2020-09-09 01:18:53 +0900

    Add test for Dinic using yukicoder

---
+++ verify/src/bin/dinic_test.rs
@@ -0,0 +1,45 @@
+use proconio::input;
+use proconio::marker::Usize1;
+use ralgo::flow::dinic::Dinic;
+// verify-helper: PROBLEM https://yukicoder.me/problems/177
+
+fn main() {
+    input! {
+        w: i32,
+        n: usize,
+        js: [i32; n],
+        m: usize,
+        cs: [i32; m],
+        qs: [[Usize1]; m],
+    }
+    let mut dinic = Dinic::new();
+    let jv: Vec<_> = (0..n).collect();
+    let cv: Vec<_> = (n..(n + m)).collect();
+    let s = n + m;
+    let t = s + 1;
+
+    for i in 0..n {
+        dinic.add_edge(s, jv[i], js[i]);
+    }
+    for i in 0..m {
+        dinic.add_edge(cv[i], t, cs[i]);
+    }
+    let mut matchs = vec![vec![true; n]; m];
+    for i in 0..m {
+        for &j in &qs[i] {
+            matchs[i][j] = false;
+        }
+    }
+    for i in 0..m {
+        for j in 0..n {
+            if matchs[i][j] {
+                dinic.add_edge(jv[j], cv[i], w);
+            }
+        }
+    }
+    if dinic.augment(s, t, w) == w {
+        println!("SHIROBAKO")
+    } else {
+        println!("BANSAKUTSUKITA")
+    }
+}
